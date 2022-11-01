8af7d3233a16ef8dc00468eb9cc0de3d1a91a090
LucyIbe <ibelucyifeosoluchukwu@gmail.com>
2022-11-01 02:13:33 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Lucia
+ */
+
+#include "main.h"
+
+/**
+ * _mencpy - copies @n bytes from the memory area pointed
+ * to by @src into that pointed to by @dest
+ * @dest: A pointer to the memory area to copy @src into
+ * @src: The souce bbuffer to copy characters from
+ * @n:the number of bytes to copy from @src
+ *
+ * Return: A pointerto the destination  buffer @dest.
+ */
+void *_mencpy(void *dest, const void *src, size_t n)
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned  char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
