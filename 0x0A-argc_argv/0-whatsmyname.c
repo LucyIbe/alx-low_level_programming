3929c8533b07d3e93f20979c510c9c9a3da39b59
LucyIbe <ibelucyifeosoluchukwu@gmail.com>
2022-11-08 22:14:04 +0000

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * @argc: the count of the arguments passed
+ * @argv: the array of the arguments passed
+ * Return: 0 (Always)
+ */
+int main(int argc, char *argv[])
+{
+		printf("%s\n", argv[argc - argc]);
+		return (0);
+}
